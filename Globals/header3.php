c237d2eda545e4dc3dc84ede75a2966b3c74c19a
AndreeaJinga <andreea.jinga@yahoo.com>
2022-04-10 21:08:04 +0300

    header done + login metadata modified

---
+++ Globals/header3.php
@@ -0,0 +1,57 @@
+<header class="headerClass">
+    <div class="navigation">
+        <img src="../Globals/logo-Crop-menuBar.png" alt="Logo" width="173" height="98">
+        <ul>
+            <li class="list">
+                <a href="#">
+                    <span class="icon"> 
+                        <ion-icon name="home-outline"></ion-icon> 
+                    </span>
+                    <span class="text">Home</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon"> 
+                        <ion-icon name="heart-outline"></ion-icon> 
+                    </span>
+                    <span class="text">Favorites</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon"> 
+                        <ion-icon name="newspaper-outline"></ion-icon>
+                    </span>
+                    <span class="text">News</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon"> 
+                        <ion-icon name="book-outline"></ion-icon>
+                    </span>
+                    <span class="text">Blog</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon"> 
+                        <ion-icon name="call-outline"></ion-icon> 
+                    </span>
+                    <span class="text">Contact</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon"> 
+                        <ion-icon name="person-outline"></ion-icon>
+                    </span>
+                    <span class="text">Your account</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</header>
