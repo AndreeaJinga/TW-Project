438f29db38677fe53ffe80edb1f0f38d338cf58e
PopescuAndreiGeorge <popescu.andreig333@gmail.com>
2022-04-11 14:49:29 +0300

    Home Page

---
+++ Home page/mapScript.js
@@ -0,0 +1,6 @@
+
+    var mySVG = document.getElementById('SVG_map');
+    mySVG.setAttribute("viewBox", "0 0 1000 705");
+    mySVG.setAttribute("stroke", "white");
+    mySVG.setAttribute("stroke-width","2");
+    mySVG.setAttribute("fill","black");
