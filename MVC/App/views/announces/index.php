c2c45aacc31aa7a779324007a477d3f645db3cee
AndreeaJinga <andreea.jinga@yahoo.com>
2022-06-12 16:20:10 +0300

    header added

--- MVC/App/views/announces/index.php
+++ MVC/App/views/announces/index.php
@@ -25,93 +25,15 @@
     </head>
 
     <body>
-        <!-- <header class="headerClass">
-            <div class="navigation">
-                <img src="../Globals/logo-Crop-menuBar.png" alt="Logo" width="173" height="98">
-                <ul class="navList">
-                        <li class="listItem">
-                            <a href="../HomePage/HomePage.html">
-                                <span class="icon"> 
-                                    <ion-icon name="home-outline"></ion-icon> 
-                                </span>
-                                <span class="text">Home</span>
-                            </a>
-                        </li>
-                        <li class="listItem">
-                            <a href="../Favorites/FavoritesPage.html">
-                                <span class="icon"> 
-                                    <ion-icon name="heart-outline"></ion-icon> 
-                                </span>
-                                <span class="text">Favorites</span>
-                            </a>
-                        </li>
-                        <li class="listItem">
-                            <a href="../AnnouncesPage/Announces.html">
-                                <span class="icon"> 
-                                    <ion-icon name="newspaper-outline"></ion-icon>
-                                </span>
-                                <span class="text">Announces</span>
-                            </a>
-                        </li>
-                        <li class="listItem">
-                            <a href="../BlogPage/blog.html">
-                                <span class="icon"> 
-                                    <ion-icon name="book-outline"></ion-icon>
-                                </span>
-                                <span class="text">Blog</span>
-                            </a>
-                        </li>
-                        <li class="listItem">
-                            <a href="../ContactPage/Contact.html">
-                                <span class="icon"> 
-                                    <ion-icon name="call-outline"></ion-icon> 
-                                </span>
-                                <span class="text">Contact</span>
-                            </a>
-                        </li>
-                        <li class="listItem">
-                            <a href="../AboutUsPage/aboutUs.html">
-                                <span class="icon"> 
-                                    <ion-icon name="people-outline"></ion-icon> 
-                                </span>
-                                <span class="text">About Us</span>
-                            </a>
-                        </li>
-                        <li class="listItem">
-                            <a href="../AccountPage/myAccount.html">
-                                <span class="icon"> 
-                                    <ion-icon name="person-outline"></ion-icon>
-                                </span>
-                                <span class="text">My account</span>
-                            </a>
-                        </li>
-                        <li class="listItem">
-                            <a href="../LoginPage/login.html">
-                                <span class="icon"> 
-                                    <ion-icon name="log-in-outline"></ion-icon>
-                                </span>
-                                <span class="text">Login</span>
-                            </a>
-                        </li>
-                    </ul>
-                <div class="burgerMenu">
-                    <div class="line line1"></div>
-                    <div class="line line2"></div>
-                    <div class="line line3"></div>
-                </div>
-            </div>
-        </header>
-            
-        <script>
-            document.addEventListener('DOMContentLoaded', nav)
-            function nav(){
-                const burger = document.querySelector('.burgerMenu');
-                const nav = document.querySelector('.navigation');
-                burger.addEventListener('click', ()=>{
-                    nav.classList.toggle('show')
-                })
+        <?php
+            $temp = realpath('resources/header3.php');
+            if($temp != false) {
+                include realpath('resources/header3.php');
             }
-        </script> -->
+            else {        
+                include realpath('../../../Public/resources/header3.php');
+            }
+        ?>
 
     <div class="Announces">
         <div id="bg"></div>
